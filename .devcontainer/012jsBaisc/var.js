919110b3e515bd0c29346992aa89e5c4d85c07a7
Isha Thoke <thokeshashank@gmail.com>
2026-05-30 14:14:37 +0000

    varaible

---
+++ .devcontainer/012jsBaisc/var.js
@@ -0,0 +1,17 @@
+const accoutId = 13234                
+let accoutEmail = "gitvh234@.com"
+var accoutPassword ="12344"
+accoutCity = "Bhopal"
+let accoutState;
+
+//accoutId =56
+accoutEmail = "hjuf23@.com"
+accoutPassword = "67"
+accoutCity = "Delhi"
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table({accoutId,accoutEmail,accoutPassword,accoutCity,accoutState})
