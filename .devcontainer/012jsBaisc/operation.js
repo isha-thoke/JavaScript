16868c856785aa20b4123f60a0b2a4f6284dbbff
Isha Thoke <thokeshashank@gmail.com>
2026-05-31 03:09:13 +0000

    Operation code

---
+++ .devcontainer/012jsBaisc/operation.js
@@ -0,0 +1,16 @@
+console.log(2+2)
+console.log(2-2)
+console.log(2%2)
+console.log(2/2)
+console.log(2*2)
+
+let str1 = "hello "
+let str2 = " Isha"
+let str3 = str1 + str2
+//console.log(str3);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
