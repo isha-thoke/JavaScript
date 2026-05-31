0648f5d7bcc90458b83c5eceb41b97ec49065cef
Isha Thoke <thokeshashank@gmail.com>
2026-05-31 03:24:43 +0000

    Comparisons Data Type

---
+++ .devcontainer/012jsBaisc/comparison.js
@@ -0,0 +1,10 @@
+console.log(2<3);
+console.log(3==3);
+console.log(5!=4);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log("2" == 2);
+console.log("2" === 2);
