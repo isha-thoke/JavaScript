a0a053862c3b2e7cecf53f89a9ec94a25dcdc200
Isha Thoke <thokeshashank@gmail.com>
2026-05-31 04:06:19 +0000

    Primitive data type

---
+++ .devcontainer/012jsBaisc/primitive.js
@@ -0,0 +1,30 @@
+//Primitive 
+
+const score = false;
+const number =100;
+
+let userEmail;
+
+const id = Symbol('123')
+
+const bigNumber = 23356643221n;
+//console.table([bigNumber,score,number,userEmail]);
+
+//7 Type : String , Number , Boolean , Null, undefine , Symbol , BigInt
+
+//Reference(Non primitive) = Array , objects , functions
+
+const heros = ["Isha" ,"Shashank" , "Shaktiman", "naagraj"]
+
+let myObj = {
+    name: "Isha",
+    age: 34,
+
+}
+
+const myFunction = function (){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
+console.log(typeof myFunction);
