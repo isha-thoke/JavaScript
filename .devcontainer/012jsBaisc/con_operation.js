ed1179cc0c1ebe797d1977ce87295fad17b64ce0
Isha Thoke <thokeshashank@gmail.com>
2026-05-31 01:13:19 +0000

    conversion of DataType

---
+++ .devcontainer/012jsBaisc/con_operation.js
@@ -0,0 +1,20 @@
+let score = "33"
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+//"33" => 33
+// "33abc" =>NaN  (Not In Number)
+//true => 1 ; flase => 0
+
+let isLogged = 1
+let booleanIsLogged = Boolean(isLogged)
+console.log(booleanIsLogged)
+
+//"" => flase
+//"Isha" => true
+
+let someNumber = 12
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
