f07c47672939b0915a24b23a9b0ed6a2940eccd4
Isha Thoke <thokeshashank@gmail.com>
2026-05-31 00:35:17 +0000

    DataType

---
+++ .devcontainer/012jsBaisc/dataType.js
@@ -0,0 +1,25 @@
+"use strict";  //treat all JS code as newer version
+
+//alert(6+6) we are using nodejs, not brower
+
+console.log(4
+    +
+4)    // code redability should be high
+console.log("Isha")
+let name = "Isha"
+let age = 21
+let isLogged = true
+
+//number => 2  to power 53
+//bigint
+//String
+//boolean => true/false
+//null =>standalone value
+//undefined => value not assin
+//symbol => uniqe
+
+//object
+
+console.log(typeof "Isha");
+console.log(typeof null);
+console.log(typeof undefined);
