fab9bf260d10b8340e0e37fab6d66e1a36d086a6
Isha Thoke <thokeshashank@gmail.com>
2026-06-01 06:02:53 +0000

    memory

---
+++ .devcontainer/012jsBaisc/memory.js
@@ -0,0 +1,19 @@
+//**************************************************** 
+//stack (Primitive type )  , Heap(Non - Primitive type)
+
+let myYoutubename = "thokeishacom";
+let another = "thokeneha";
+another = "thokeshashank";
+
+console.log(myYoutubename);
+console.log(another);
+
+let userOne = {
+email : "fgs12@goolge.com",
+upi : "user34"
+}
+
+let user =userOne;
+user.email = "hij127@goolge.com"
+console.log(userOne.email);
+console.log(user.email);
