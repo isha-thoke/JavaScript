0ad61ee38ed711e7577c6383a3f55ad5bc4f106a
Isha Thoke <thokeshashank@gmail.com>
2026-06-01 07:57:56 +0000

    String in Java Script

---
+++ .devcontainer/012jsBaisc/String.js
@@ -0,0 +1,18 @@
+const name = "   Isha   ";
+const repoCount =89;
+
+//console.log( "name is " + name + "my Repo count "+ repoCount );
+//console.log(`hello my name is ${name}  and my repo is count ${repoCount}`);
+
+const gameName = new String('IS_game')
+//console.log(gameName.__proto__);
+
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+//console.log(gameName.toLowerCase());
+//console.log(gameName.charAt(3));
+//console.log(gameName.indexOf('S'))
+//console.log(gameName.substring(0,3));
+//console.log(name.trim());
+const url = "https://isha.com/isha%20ishai";
+console.log(url.replace('%20' , '_'));
