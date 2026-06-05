415df782d829e8a96a268e8eef9cb05fd4cd01be
Isha Thoke <thokeshashank@gmail.com>
2026-06-05 15:50:43 +0000

    Number in operation Maths

---
+++ .devcontainer/012jsBaisc/numberMaths.js
@@ -0,0 +1,22 @@
+const score = 789
+
+const balance  = new Number(100)
+console.log(balance);
+console.log(score);
+console.log(balance.toString().length);
+console.log(balance.toFixed(3));
+console.log(balance.toPrecision(2))
+
+const hun = 1000000
+console.log(hun.toLocaleString('en-In'))
+
+
+//++++++++++++++++Maths+++++++++++++++++++++++
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(3.2));
+console.log(Math.floor(6.4));
+console.log(Math.min(3,4,6));
+console.log(Math.max(4,6,3));
+console.log(Math.random());
